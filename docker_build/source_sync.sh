83e62bd4f396eadfc5cc7e7eb49729a56da05448
SpootDev <spootdev@gmail.com>
2021-07-17 14:09:51 -0500

    fix lib path

--- docker_build/source_sync.sh
+++ docker_build/source_sync.sh
@@ -2,5 +2,5 @@
 
 # mknotify
 cp ../src/mk_lib_database/src/mk_lib_database.rs ../docker/core/mkinotify/src/.
-cp ../src/mk_lib_database_library/src/mk_lib_database_library.rs ../docker/core/mkinotify/src/.
+cp ../src/mk_lib_database/src/mk_lib_database_library.rs ../docker/core/mkinotify/src/.
 cp ../src/mk_lib_logging/src/mk_lib_logging.rs ../docker/core/mkinotify/src/.
