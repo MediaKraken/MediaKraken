1369ff8c6d815de214e3d657576abfe37e337f94
SpootDev <spootdev@gmail.com>
2021-07-17 14:07:02 -0500

    source sync

--- docker_build/source_sync.sh
+++ docker_build/source_sync.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# mknotify
+cp ../../src/mk_lib_database/src/mk_lib_database.rs ../../docker/core/mkinotify/src/.
+cp ../../src/mk_lib_database_library/src/mk_lib_database_library.rs ../../docker/core/mkinotify/src/.
 cp ../../src/mk_lib_logging/src/mk_lib_logging.rs ../../docker/core/mkinotify/src/.
