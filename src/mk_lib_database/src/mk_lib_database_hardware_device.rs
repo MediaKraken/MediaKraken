afca2806b442bbc97bd45418dfdd17f1f61f1331
SpootDev <spootdev@gmail.com>
2021-07-31 12:48:39 -0500

    fix conflict

--- src/mk_lib_database/src/mk_lib_database_hardware_device.rs
+++ src/mk_lib_database/src/mk_lib_database_hardware_device.rs
@@ -3,12 +3,13 @@ use uuid::Uuid;
 
 pub async fn mk_lib_database_hardware_manufacturer_upsert(client: &tokio_postgres::Client,
                                                           manufacturer_name: String,
-                                                          manufacturer_id: i16)
+                                                          manufacturer_id: i32)
                                                           -> Result<(), Error> {
+    println!("{:?} {:?}", manufacturer_name, manufacturer_id);
     client
-        .query("INSERT INTO mm_hardware_manufacturer (mm_hardware_manu_guid,\
-         mm_hardware_manu_name, mm_hardware_manu_gc_id) VALUES($1, $2, $3)\
-          ON CONFLICT (mm_hardware_manu_name) DO NOTHING;",
-               &[&Uuid::new_v4(), &manufacturer_name, &manufacturer_id]).await?;
+        .query("insert into mm_hardware_manufacturer (mm_hardware_manu_guid, \
+        mm_hardware_manu_name, mm_hardware_manu_gc_id) values ($1, $2, $3) \
+        ON CONFLICT (mm_hardware_manu_name) DO NOTHING",
+                   &[&Uuid::new_v4(), &manufacturer_name, &manufacturer_id]).await?;
     Ok(())
 }
