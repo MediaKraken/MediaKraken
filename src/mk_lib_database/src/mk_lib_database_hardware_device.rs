aa150c9c2c3b0710b440fe2a76ebb7ee33d62e60
SpootDev <spootdev@gmail.com>
2021-07-31 19:12:53 -0500

    debug

--- src/mk_lib_database/src/mk_lib_database_hardware_device.rs
+++ src/mk_lib_database/src/mk_lib_database_hardware_device.rs
@@ -5,7 +5,7 @@ pub async fn mk_lib_database_hardware_manufacturer_upsert(client: &tokio_postgre
                                                           manufacturer_name: String,
                                                           manufacturer_id: i32)
                                                           -> Result<(), Error> {
-    println!("{:?} {:?}", manufacturer_name, manufacturer_id);
+    println!("here {:?} {:?}", manufacturer_name, manufacturer_id);
     client
         .query("insert into mm_hardware_manufacturer (mm_hardware_manu_guid, \
         mm_hardware_manu_name, mm_hardware_manu_gc_id) values ($1, $2, $3) \
@@ -17,7 +17,7 @@ pub async fn mk_lib_database_hardware_manufacturer_upsert(client: &tokio_postgre
 pub async fn mk_lib_database_hardware_type_upsert(client: &tokio_postgres::Client,
                                                   hardware_type: String)
                                                   -> Result<(), Error> {
-    println!("{:?}", hardware_type);
+    println!("here2 {:?}", hardware_type);
     client
         .query("insert into mm_hardware_type (mm_hardware_type_guid, \
         mm_hardware_type_name) values ($1, $2) \
