f3df355e7e89ce5b2607c8c31dab1d6f56a70bf6
spootdev <spootdev@gmail.com>
2021-08-15 18:01:30 -0500

    enum not guid

--- src/mk_lib_database/src/mk_lib_database_media.rs
+++ src/mk_lib_database/src/mk_lib_database_media.rs
@@ -3,17 +3,17 @@ use uuid::Uuid;
 
 pub async fn mk_lib_database_media_insert(client: &tokio_postgres::Client,
                                              mm_media_guid: Uuid,
-                                             mm_media_class_guid: i16,
+                                             mm_media_class_enum: i16,
                                              mm_media_path: String,
                                              mm_media_metadata_guid: Uuid,
                                              mm_media_ffprobe_json: String,
                                              mm_media_json: String)
                                              -> Result<bool, Error> {
     client
-        .query_one("insert into mm_media (mm_media_guid, mm_media_class_guid,\
+        .query_one("insert into mm_media (mm_media_guid, mm_media_class_enum,\
          mm_media_path, mm_media_metadata_guid, mm_media_ffprobe_json, mm_media_json)\
           values ($1, $2, $3, $4, $5, $6)",
-                   &[&mm_media_guid, &mm_media_class_guid, &mm_media_path,
+                   &[&mm_media_guid, &mm_media_class_enum, &mm_media_path,
                        &mm_media_metadata_guid, &mm_media_ffprobe_json, &mm_media_json]).await?;
     Ok()
 }
