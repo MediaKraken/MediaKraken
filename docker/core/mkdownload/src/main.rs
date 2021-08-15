eb75b5203ad1bac17b83bfa1b152d1f16648cc2e
spootdev <spootdev@gmail.com>
2021-08-15 15:54:05 -0500

    dl youtube

--- docker/core/mkdownload/src/main.rs
+++ docker/core/mkdownload/src/main.rs
@@ -3,6 +3,7 @@ use serde_json::{json, Value};
 use std::error::Error;
 use std::path::Path;
 use std::process::Command;
+use rustube::{Id, VideoFetcher};
 
 #[cfg(debug_assertions)]
 #[path = "../../../../src/mk_lib_database/src/mk_lib_database.rs"]
@@ -66,13 +67,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
                                                                   json_message["Local Save Path"].to_string());
                     } else if json_message["Type"].to_string() == "Youtube" {
                         if validator::validate_url(json_message["URL"].to_string()) {
-                            Command::new("youtube-dl")
-                                .arg("-i")
-                                .arg("--download-archive")
-                                .arg("/mediakraken/downloads/yt_dl_archive.txt")
-                                .arg(json_message["URL"].to_string())
-                                .spawn()
-                                .expect("youtube-dl command failed to start");
+                            //let url = "https://www.youtube.com/watch?v=Edx9D2yaOGs&ab_channel=CollegeHumor";
+                            println!("downloaded video to {:?}", rustube::download_best_quality(&json_message["URL"].to_string()).await.unwrap());
+                            // Command::new("youtube-dl")
+                            //     .arg("-i")
+                            //     .arg("--download-archive")
+                            //     .arg("/mediakraken/downloads/yt_dl_archive.txt")
+                            //     .arg(json_message["URL"].to_string())
+                            //     .spawn()
+                            //     .expect("youtube-dl command failed to start");
                         } else {
                             // TODO log error by user requested
                             continue;
