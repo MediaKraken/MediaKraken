932dc3fed4d25c48ea925c8f319c65c69d7adefd
spootdev <spootdev@gmail.com>
2021-08-30 18:32:58 -0500

    hplaptop to other db

--- src/mk_lib_database/src/mk_lib_database.rs
+++ src/mk_lib_database/src/mk_lib_database.rs
@@ -6,16 +6,18 @@ use sqlx::postgres::PgPoolOptions;
 pub async fn mk_lib_database_open_pool() -> Result<sqlx::PgPool, sqlx::Error> {
     // trim is get rid of the \r returned in hostname
     let hostname: String = sys_info::hostname().unwrap().trim().to_string();
-    let connection_string: String;
+    let mut connection_string: String;
     if hostname == "wsripper2" {
-        connection_string = "postgresql://postgres:metaman@localhost/postgres".to_string();
+         connection_string = "postgresql://postgres:metaman@localhost/postgres".to_string();
+    } else if hostname == "th-hplaptop-1" {
+         connection_string = "postgresql://postgres:metaman@th-postgresql-1/postgres".to_string();
     } else if Path::new("/run/secrets/db_password").exists() {
         let dp_pass = fs::read_to_string("/run/secrets/db_password").unwrap();
-        connection_string = format!("postgresql://postgres:{}@mkstack_database/postgres",
+         connection_string = format!("postgresql://postgres:{}@mkstack_database/postgres",
                                     dp_pass);
     } else {
         let dp_pass = env::var("POSTGRES_PASSWORD").unwrap();
-        connection_string = format!("postgresql://postgres:{}@mkstack_database/postgres",
+         connection_string = format!("postgresql://postgres:{}@mkstack_database/postgres",
                                     dp_pass);
     }
     let pool = PgPoolOptions::new()
