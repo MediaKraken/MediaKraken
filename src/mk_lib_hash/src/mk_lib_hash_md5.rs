fa6b34d2df6e6aec3c7d44abadb4174ff2ea4999
spootdev <spootdev@gmail.com>
2021-09-01 18:20:47 -0500

    dyn

--- src/mk_lib_hash/src/mk_lib_hash_md5.rs
+++ src/mk_lib_hash/src/mk_lib_hash_md5.rs
@@ -2,7 +2,7 @@ use md5::{Md5, Digest};
 use std::fs;
 use std::error::Error;
 
-pub fn mk_file_hash_md5(file_to_read: &str) -> Result<String, dyn Error> {
+pub fn mk_file_hash_md5(file_to_read: &str) -> Result<String, Error> {
     let mut hasher = Md5::new();
 
     let mut file = fs::File::open(&file_to_read)?;
