1b00d234a5c33fe40cc63123175137bd8059d9f6
spootdev <spootdev@gmail.com>
2021-09-01 18:21:06 -0500

    return jsons

--- src/mk_lib_database/src/mk_lib_database_option_status.rs
+++ src/mk_lib_database/src/mk_lib_database_option_status.rs
@@ -2,21 +2,19 @@ use uuid::Uuid;
 use sqlx::postgres::PgRow;
 
 pub async fn mk_lib_database_option_read(pool: &sqlx::PgPool)
-                                         -> Result<Vec<PgRow>, sqlx::Error> {
-    let rows = sqlx::query("select mm_options_json \
-        from mm_options_and_status")
-        .fetch_all(pool)
+                                         -> Result<serde_json::Value, sqlx::Error> {
+    let row: (serde_json::Value, ) = sqlx::query_as("select mm_options_json from mm_options_and_status")
+        .fetch_one(pool)
         .await?;
-    Ok(rows)
+    Ok(row.0)
 }
 
 pub async fn mk_lib_database_status_read(pool: &sqlx::PgPool)
-                                         -> Result<Vec<PgRow>, sqlx::Error> {
-    let rows = sqlx::query("select mm_status_json \
-        from mm_options_and_status")
-        .fetch_all(pool)
+                                         -> Result<serde_json::Value, sqlx::Error> {
+    let row: (serde_json::Value, ) = sqlx::query_as("select mm_status_json from mm_options_and_status")
+        .fetch_one(pool)
         .await?;
-    Ok(rows)
+    Ok(row.0)
 }
 
 pub async fn mk_lib_database_option_status_read(pool: &sqlx::PgPool)
