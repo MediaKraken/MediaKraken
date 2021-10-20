9514b0396927ec49e8ce8867e58b4f075aa66301
SpootDev <spootdev@gmail.com>
2021-10-20 18:26:46 -0500

    fix dl and add cargo zip

--- docker/core/mktmdbnetfetchbulk/src/main.rs
+++ docker/core/mktmdbnetfetchbulk/src/main.rs
@@ -99,9 +99,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // grab the movie id's
     // files.tmdb.org = 13.227.42.62
-    // let _fetch_result_movie = mk_lib_network::mk_download_file_from_url(
-    //     format!("http://files.tmdb.org/p/exports/movie_ids_{}.json.gz", fetch_date),
-    //     "/mediakraken/movie.gz".to_string()).await;
+    let _fetch_result_movie = mk_lib_network::mk_download_file_from_url(
+        format!("http://files.tmdb.org/p/exports/movie_ids_{}.json.gz", fetch_date),
+        &"/mediakraken/movie.gz".to_string()).await;
     let json_result = mk_lib_compression::mk_decompress_gz_data("/mediakraken/movie.gz").unwrap();
     // Please note that the data is NOT in id order
     for json_item in json_result.split('\n') {
@@ -130,7 +130,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // grab the TV id's
     let _fetch_result_tv = mk_lib_network::mk_download_file_from_url(
         format!("http://files.tmdb.org/p/exports/tv_series_ids_{}.json.gz", fetch_date),
-        "/mediakraken/tv.gz".to_string()).await;
+        &"/mediakraken/tv.gz".to_string()).await;
     let json_result = mk_lib_compression::mk_decompress_gz_data("/mediakraken/tv.gz").unwrap();
     for json_item in json_result.split('\n') {
         if !json_item.trim().is_empty() {
