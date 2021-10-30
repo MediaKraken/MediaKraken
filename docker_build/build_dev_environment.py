d29d040dd00590c1df7f9e61b8fc8f28d9a1a42e
SpootDev <spootdev@gmail.com>
2021-10-30 10:24:46 -0500

    write output and do waits

--- docker_build/build_dev_environment.py
+++ docker_build/build_dev_environment.py
@@ -4,22 +4,33 @@ import shlex
 # shouldn't need to build out Rust since that's container build
 # TODO do I really need a registry for the test/dev environment?
 
+
+def subprocess_run(command_string):
+    pid_build_proc = subprocess.Popen(shlex.split(command_string),
+                                      stdout=subprocess.PIPE, shell=False)
+    while True:
+        line = pid_build_proc.stdout.readline()
+        if not line:
+            break
+        print(line.rstrip(), flush=True)
+    pid_build_proc.wait()
+
+
 # build out docker and docker-compose
-subprocess.call(shlex.split('python3 ../docker_compose/mediakraken_setup.py'),
-                stdout=subprocess.PIPE, shell=False)
+print("build out docker and docker-compose")
+subprocess_run(shlex.split('python3 ../docker_compose/mediakraken_setup.py'))
 
 # build the jenkins build for mediakraken
-subprocess.call(shlex.split('docker build ../docker/test/mkjenkins/. -t mkjenkins'),
-                stdout=subprocess.PIPE, shell=False)
+print("build the jenkins build for mediakraken")
+subprocess_run(shlex.split('docker build ../docker/test/mkjenkins/. -t mkjenkins'))
 
 # TODO setup the mailcow
-# subprocess.call(shlex.split('python3 ../docker_compose/mediakraken_setup.py'),
-#                 stdout=subprocess.PIPE, shell=False)
+# subprocess_run(shlex.split('python3 ../docker_compose/mediakraken_setup.py'))
 
 # selenium mediakraken test image
-subprocess.call(shlex.split('docker build ../docker/test/mkselenium/. -t mkselenium'),
-                stdout=subprocess.PIPE, shell=False)
+print("selenium mediakraken test image")
+subprocess_run(shlex.split('docker build ../docker/test/mkselenium/. -t mkselenium'))
 
 # trac instance for bug tracking
-subprocess.call(shlex.split('docker build ../docker/test/mktrac/. -t mktrac'),
-                stdout=subprocess.PIPE, shell=False)
+print("trac instance for bug tracking")
+subprocess_run(shlex.split('docker build ../docker/test/mktrac/. -t mktrac'))
