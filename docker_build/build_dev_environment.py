4480ef7a39eb00e36705daa117ec48bdb9fb3481
SpootDev <spootdev@gmail.com>
2021-12-26 23:01:51 -0600

    deploy dir

--- docker_build/build_dev_environment.py
+++ docker_build/build_dev_environment.py
@@ -22,10 +22,10 @@ working_directory = os.getcwd().replace("docker_build", "")
 
 print("Path:", os.getcwd())
 # build dir(s) to hold mediakraken data (docker data)
-os.makedirs("/var/opt/mediakraken/sonatype", exist_ok=True)
+os.makedirs("/var/opt/mediakraken/sonatype/deploy", exist_ok=True)
 print("Path:", os.getcwd())
 shutil.copytree(os.path.join(working_directory, "docker/test/mksonatype/deploy"),
-                         "/var/opt/mediakraken/sonatype", dirs_exist_ok=True)
+                         "/var/opt/mediakraken/sonatype/deploy", dirs_exist_ok=True)
 
 print("Path:", os.getcwd())
 os.makedirs("/var/opt/mediakraken/trac/projects", exist_ok=True)
