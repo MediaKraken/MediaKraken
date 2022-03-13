bd6e21e189eb2862908e791a40b3a69114c8762b
SpootDev <spootdev@gmail.com>
2022-03-13 17:45:05 -0500

    template path

--- docker/core/mkwebapp/src/public/bp_about.rs
+++ docker/core/mkwebapp/src/public/bp_about.rs
@@ -4,14 +4,14 @@ use rocket_dyn_templates::{Template, tera::Tera, context};
 
 #[get("/about")]
 pub fn about() -> Template {
-    Template::render("tera/about.html", context! {
+    Template::render("templates/bbs_public_about.html", context! {
         title: "About",
     })
 }
 
 // @common_global.jinja_template.template('bss_public/bss_public_about.html')
 pub fn customize(tera: &mut Tera) {
-    tera.add_raw_template("tera/about.html", r#"
+    tera.add_raw_template("templates/bbs_public_about.html", r#"
         {% extends "tera/base" %}
         {% block content %}
             <section id="about">
