4e0dff05f076e3aed35276775be4c18cb16ed5fe
SpootDev <SpootDev@users.noreply.github.com>
2022-03-27 16:17:39 -0500

    change db connection for testing on windows

--- docker/core/mkmetadatamame/src/mk_lib_database.rs
+++ docker/core/mkmetadatamame/src/mk_lib_database.rs
@@ -8,9 +8,7 @@ pub async fn mk_lib_database_open_pool() -> Result<sqlx::PgPool, sqlx::Error> {
     let hostname: String = sys_info::hostname().unwrap().trim().to_string();
     println!("Host: {}", hostname);
     let mut connection_string: String;
-    if hostname == "wsripper2" {
-         connection_string = "postgresql://postgres:metaman@localhost/postgres".to_string();
-    } else if hostname == "th-hplaptop-1" || hostname == "th-hplap-1"
+    if hostname == "wsripper2" || hostname == "th-hplaptop-1" || hostname == "th-hplap-1"
         || hostname == "th-linuxgui-1" || hostname == "mkstage" {
          connection_string = "postgresql://postgres:metaman@mkstage/postgres".to_string();
     } else if Path::new("/run/secrets/db_password").exists() {
