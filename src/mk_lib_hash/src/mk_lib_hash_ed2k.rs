9924a06af7bf94cb9d76697dbd0e3dd9e224a5ab
SpootDev <spootdev@gmail.com>
2022-04-10 13:08:59 -0500

    ed2k

---
+++ src/mk_lib_hash/src/mk_lib_hash_ed2k.rs
@@ -0,0 +1,7 @@
+// https://github.com/runfalk/ed2k-rs
+
+pub fn mk_file_hash_ed2k(file_to_read: &str)
+                         -> Result<String, Box<dyn Error>> {
+    let ed2k = Ed2k::from_path(file_to_read)?;
+    return ed2k
+}
