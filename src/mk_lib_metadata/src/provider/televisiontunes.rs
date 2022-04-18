2ce3154c56445392b4721976d359ef23ee81f432
SpootDev <spootdev@gmail.com>
2022-04-17 20:02:57 -0500

    notes

--- src/mk_lib_metadata/src/provider/televisiontunes.rs
+++ src/mk_lib_metadata/src/provider/televisiontunes.rs
@@ -3,4 +3,9 @@
 #[path = "../../mk_lib_network.rs"]
 mod mk_lib_network;
 
+// https://www.televisiontunes.com/Saving_Grace.html
+// so, replace space with _ and go to url?
+// <p class="media480 m_down mm"><a id="download_song" href="/song/download/4601">Download Saving Grace </a></p>
+// pull the href and add to base url, and download
+
 pub async fn provider_televisiontunes_theme_fetch(pool: &sqlx::PgPool, tv_show_name: String) {}
