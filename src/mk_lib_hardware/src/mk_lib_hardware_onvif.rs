85fe268f95e945aaa840736163257ed2f7e07bbe
spootdev <spootdev@gmail.com>
2022-07-30 12:41:36 -0500

    onvif discovery

--- src/mk_lib_hardware/src/mk_lib_hardware_onvif.rs
+++ src/mk_lib_hardware/src/mk_lib_hardware_onvif.rs
@@ -1,1 +1,18 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+
+// https://github.com/lumeohq/onvif-rs
+// onvif = { git = "https://github.com/lumeohq/onvif-rs" }
+
+use onvif::discovery;
+
+const MAX_CONCURRENT_JUMPERS: usize = 100;
+
+pub async fn mk_lib_hardware_onvif_discovery() {
+    discovery::discover(std::time::Duration::from_secs(1))
+        .await
+        .unwrap()
+        .for_each_concurrent(MAX_CONCURRENT_JUMPERS, |addr| async move {
+            println!("Device found: {:?}", addr);
+        })
+        .await;
+}
