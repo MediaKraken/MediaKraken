645ea4cc5f5b79235a50ffb9dd0c94157e4c08b5
spootdev <spootdev@gmail.com>
2023-04-09 14:06:02 -0500

    test docker  and works

---
+++ testing_rust/test_axum_auth/dock_build.sh
@@ -0,0 +1,1 @@
+docker build -t mediakraken/mkexample:dev .
--- testing_rust/test_axum_auth/src/main.rs
+++ testing_rust/test_axum_auth/src/main.rs
@@ -8,6 +8,9 @@ use std::{collections::HashSet};
 use tokio::signal;
 use chrono::prelude::*;
 
+#[path = "mk_lib_database_user.rs"]
+mod mk_lib_database_user;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     pub id: i64,
@@ -19,168 +22,168 @@ pub struct User {
     pub permissions: HashSet<String>,
 }
 
-#[derive(sqlx::FromRow, Clone)]
-pub struct SqlPermissionTokens {
-    pub token: String,
-}
+// #[derive(sqlx::FromRow, Clone)]
+// pub struct SqlPermissionTokens {
+//     pub token: String,
+// }
 
-impl Default for User {
-    fn default() -> Self {
-        let mut permissions = HashSet::new();
+// impl Default for User {
+//     fn default() -> Self {
+//         let mut permissions = HashSet::new();
 
-        permissions.insert("Category::View".to_owned());
+//         permissions.insert("Category::View".to_owned());
 
-        Self {
-            id: 1,
-            anonymous: true,
-            username: "Guest".into(),
-            email: "fake@fake.com".into(),
-            last_signin: Utc::now(),
-            last_signoff: Utc::now(),            
-            permissions: permissions,
-        }
-    }
-}
+//         Self {
+//             id: 1,
+//             anonymous: true,
+//             username: "Guest".into(),
+//             email: "fake@fake.com".into(),
+//             last_signin: Utc::now(),
+//             last_signoff: Utc::now(),            
+//             permissions: permissions,
+//         }
+//     }
+// }
 
-#[async_trait]
-impl Authentication<User, i64, PgPool> for User {
-    async fn load_user(userid: i64, pool: Option<&PgPool>) -> Result<User, anyhow::Error> {
-        let pool = pool.unwrap();
+// #[async_trait]
+// impl Authentication<User, i64, PgPool> for User {
+//     async fn load_user(userid: i64, pool: Option<&PgPool>) -> Result<User, anyhow::Error> {
+//         let pool = pool.unwrap();
 
-        User::get_user(userid, pool)
-            .await
-            .ok_or_else(|| anyhow::anyhow!("Could not load user"))
-    }
+//         User::get_user(userid, pool)
+//             .await
+//             .ok_or_else(|| anyhow::anyhow!("Could not load user"))
+//     }
 
-    fn is_authenticated(&self) -> bool {
-        !self.anonymous
-    }
+//     fn is_authenticated(&self) -> bool {
+//         !self.anonymous
+//     }
 
-    fn is_active(&self) -> bool {
-        !self.anonymous
-    }
+//     fn is_active(&self) -> bool {
+//         !self.anonymous
+//     }
 
-    fn is_anonymous(&self) -> bool {
-        self.anonymous
-    }
-}
+//     fn is_anonymous(&self) -> bool {
+//         self.anonymous
+//     }
+// }
 
-#[async_trait]
-impl HasPermission<PgPool> for User {
-    async fn has(&self, perm: &str, _pool: &Option<&PgPool>) -> bool {
-        self.permissions.contains(perm)
-    }
-}
+// #[async_trait]
+// impl HasPermission<PgPool> for User {
+//     async fn has(&self, perm: &str, _pool: &Option<&PgPool>) -> bool {
+//         self.permissions.contains(perm)
+//     }
+// }
 
-impl User {
-    pub async fn get_user(id: i64, pool: &PgPool) -> Option<Self> {
-        let sqluser = sqlx::query_as::<_, SqlUser>("SELECT * FROM users WHERE id = $1")
-            .bind(id)
-            .fetch_one(pool)
-            .await
-            .ok()?;
+// impl User {
+//     pub async fn get_user(id: i64, pool: &PgPool) -> Option<Self> {
+//         let sqluser = sqlx::query_as::<_, SqlUser>("SELECT * FROM users WHERE id = $1")
+//             .bind(id)
+//             .fetch_one(pool)
+//             .await
+//             .ok()?;
 
-        //lets just get all the tokens the user can use, we will only use the full permissions if modifing them.
-        let sql_user_perms = sqlx::query_as::<_, SqlPermissionTokens>(
-            "SELECT token FROM user_permissions WHERE user_id = $1;",
-        )
-        .bind(id)
-        .fetch_all(pool)
-        .await
-        .ok()?;
+//         //lets just get all the tokens the user can use, we will only use the full permissions if modifing them.
+//         let sql_user_perms = sqlx::query_as::<_, SqlPermissionTokens>(
+//             "SELECT token FROM user_permissions WHERE user_id = $1;",
+//         )
+//         .bind(id)
+//         .fetch_all(pool)
+//         .await
+//         .ok()?;
 
-        Some(sqluser.into_user(Some(sql_user_perms)))
-    }
+//         Some(sqluser.into_user(Some(sql_user_perms)))
+//     }
 
-    pub async fn create_user_tables(pool: &PgPool) {
-        sqlx::query(
-            r#"
-                CREATE TABLE IF NOT EXISTS users (
-                    "id" bigint Primary Key Generated Always as Identity,
-                    "anonymous" BOOLEAN NOT NULL,
-                    "username" VARCHAR(256) NOT NULL,
-                    "email" TEXT
-                )
-            "#,
-        )
-        .execute(pool)
-        .await
-        .unwrap();
+//     pub async fn create_user_tables(pool: &PgPool) {
+//         sqlx::query(
+//             r#"
+//                 CREATE TABLE IF NOT EXISTS users (
+//                     "id" bigint Primary Key Generated Always as Identity,
+//                     "anonymous" BOOLEAN NOT NULL,
+//                     "username" VARCHAR(256) NOT NULL,
+//                     "email" TEXT
+//                 )
+//             "#,
+//         )
+//         .execute(pool)
+//         .await
+//         .unwrap();
 
-        sqlx::query(
-            r#"
-                CREATE TABLE IF NOT EXISTS user_permissions (
-                    "user_id" INTEGER NOT NULL,
-                    "token" VARCHAR(256) NOT NULL
-                )
-        "#,
-        )
-        .execute(pool)
-        .await
-        .unwrap();
+//         sqlx::query(
+//             r#"
+//                 CREATE TABLE IF NOT EXISTS user_permissions (
+//                     "user_id" INTEGER NOT NULL,
+//                     "token" VARCHAR(256) NOT NULL
+//                 )
+//         "#,
+//         )
+//         .execute(pool)
+//         .await
+//         .unwrap();
 
-        sqlx::query(
-            r#"
-                INSERT INTO users
-                    (anonymous, username) SELECT true, 'Guest'
-            "#,
-        )
-        .execute(pool)
-        .await
-        .unwrap();
+//         sqlx::query(
+//             r#"
+//                 INSERT INTO users
+//                     (anonymous, username) SELECT true, 'Guest'
+//             "#,
+//         )
+//         .execute(pool)
+//         .await
+//         .unwrap();
 
-        sqlx::query(
-            r#"
-                INSERT INTO users
-                    (anonymous, username) SELECT true, 'Test'
-            "#,
-        )
-        .execute(pool)
-        .await
-        .unwrap();
+//         sqlx::query(
+//             r#"
+//                 INSERT INTO users
+//                     (anonymous, username) SELECT true, 'Test'
+//             "#,
+//         )
+//         .execute(pool)
+//         .await
+//         .unwrap();
 
-        sqlx::query(
-            r#"
-                INSERT INTO user_permissions
-                    (user_id, token) SELECT 2, 'Category::View'
-            "#,
-        )
-        .execute(pool)
-        .await
-        .unwrap();
-    }
-}
+//         sqlx::query(
+//             r#"
+//                 INSERT INTO user_permissions
+//                     (user_id, token) SELECT 2, 'Category::View'
+//             "#,
+//         )
+//         .execute(pool)
+//         .await
+//         .unwrap();
+//     }
+// }
 
-#[derive(sqlx::FromRow, Clone)]
-pub struct SqlUser {
-    pub id: i64,
-    pub anonymous: bool,
-    pub username: String,
-    pub email: String,
-    pub last_signin: DateTime<Utc>,
-    pub last_signoff: DateTime<Utc>,    
-}
+// #[derive(sqlx::FromRow, Clone)]
+// pub struct SqlUser {
+//     pub id: i64,
+//     pub anonymous: bool,
+//     pub username: String,
+//     pub email: String,
+//     pub last_signin: DateTime<Utc>,
+//     pub last_signoff: DateTime<Utc>,    
+// }
 
-impl SqlUser {
-    pub fn into_user(self, sql_user_perms: Option<Vec<SqlPermissionTokens>>) -> User {
-        User {
-            id: self.id,
-            anonymous: self.anonymous,
-            username: self.username,
-            email: self.email,
-            last_signin: self.last_signin,
-            last_signoff: self.last_signoff,            
-            permissions: if let Some(user_perms) = sql_user_perms {
-                user_perms
-                    .into_iter()
-                    .map(|x| x.token)
-                    .collect::<HashSet<String>>()
-            } else {
-                HashSet::<String>::new()
-            },
-        }
-    }
-}
+// impl SqlUser {
+//     pub fn into_user(self, sql_user_perms: Option<Vec<SqlPermissionTokens>>) -> User {
+//         User {
+//             id: self.id,
+//             anonymous: self.anonymous,
+//             username: self.username,
+//             email: self.email,
+//             last_signin: self.last_signin,
+//             last_signoff: self.last_signoff,            
+//             permissions: if let Some(user_perms) = sql_user_perms {
+//                 user_perms
+//                     .into_iter()
+//                     .map(|x| x.token)
+//                     .collect::<HashSet<String>>()
+//             } else {
+//                 HashSet::<String>::new()
+//             },
+//         }
+//     }
+// }
 
 #[tokio::main]
 async fn main() {
@@ -195,7 +198,7 @@ async fn main() {
 
     //Create the Database table for storing our Session Data.
     session_store.initiate().await.unwrap();
-    User::create_user_tables(&pool).await;  // will run without this just fine (after first run)
+    //User::create_user_tables(&pool).await;  // will run without this just fine (after first run)
 
     // build our application with some routes
     let app = Router::new()
@@ -205,7 +208,7 @@ async fn main() {
         .route("/perm", get(perm))
         .nest("/static", axum_static::static_router("static"))        
         .layer(
-            AuthSessionLayer::<User, i64, SessionPgPool, PgPool>::new(Some(pool.clone().into()))
+            AuthSessionLayer::<mk_lib_database_user::User, i64, SessionPgPool, PgPool>::new(Some(pool.clone().into()))
                 .with_config(auth_config),
         )
         .layer(SessionLayer::new(session_store))
@@ -220,26 +223,26 @@ async fn main() {
         .unwrap();
 }
 
-async fn greet(auth: AuthSession<User, i64, SessionPgPool, PgPool>) -> String {
+async fn greet(auth: AuthSession<mk_lib_database_user::User, i64, SessionPgPool, PgPool>) -> String {
     format!(
         "Hello {}, Try logging in via /login or testing permissions via /perm",
         auth.current_user.unwrap().username
     )
 }
 
-async fn login(auth: AuthSession<User, i64, SessionPgPool, PgPool>) -> String {
+async fn login(auth: AuthSession<mk_lib_database_user::User, i64, SessionPgPool, PgPool>) -> String {
     auth.login_user(2);
     "You are logged in as a User please try /perm to check permissions".to_owned()
 }
 
 async fn perm(
     method: Method,
-    auth: AuthSession<User, i64, SessionPgPool, PgPool>,
+    auth: AuthSession<mk_lib_database_user::User, i64, SessionPgPool, PgPool>,
 ) -> String {
     let current_user = auth.current_user.clone().unwrap_or_default();
 
     //lets check permissions only and not worry about if they are anon or not
-    if !Auth::<User, i64, PgPool>::build([Method::GET], false)
+    if !Auth::<mk_lib_database_user::User, i64, PgPool>::build([Method::GET], false)
         .requires(Rights::any([
             Rights::permission("Category::View"),
             Rights::permission("Admin::View"),
