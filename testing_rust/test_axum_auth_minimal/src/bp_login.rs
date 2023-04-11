f151c2140d3a3f1af2a0f13137f93f4a77e835ad
spootdev <spootdev@gmail.com>
2023-04-10 20:04:43 -0500

    minimal

---
+++ testing_rust/test_axum_auth_minimal/src/bp_login.rs
@@ -0,0 +1,60 @@
+#![cfg_attr(debug_assertions, allow(dead_code))]
+
+use axum::{
+    http::{header, HeaderMap, Method, StatusCode},
+    response::{Html, IntoResponse, Redirect},
+    routing::{get, post},
+    Extension, Router,
+};
+use axum_session::{
+    DatabasePool, Session, SessionConfig, SessionLayer, SessionPgPool, SessionStore,
+};
+use axum_session_auth::*;
+use sqlx::{
+    postgres::{PgConnectOptions, PgPoolOptions},
+    ConnectOptions, PgPool,
+};
+
+#[path = "mk_lib_database_user.rs"]
+mod mk_lib_database_user;
+
+pub async fn greet(
+    auth: AuthSession<mk_lib_database_user::User, i64, SessionPgPool, PgPool>,
+) -> String {
+    format!(
+        "Hello {}, Try logging in via /login or testing permissions via /perm",
+        auth.current_user.unwrap().username
+    )
+}
+
+pub async fn login(
+    auth: AuthSession<mk_lib_database_user::User, i64, SessionPgPool, PgPool>,
+) -> String {
+    auth.login_user(2);
+    "You are logged in as a User please try /perm to check permissions".to_owned()
+}
+
+pub async fn perm(
+    method: Method,
+    auth: AuthSession<mk_lib_database_user::User, i64, SessionPgPool, PgPool>,
+) -> String {
+    let current_user = auth.current_user.clone().unwrap_or_default();
+    if !Auth::<mk_lib_database_user::User, i64, PgPool>::build([Method::GET], false)
+        .requires(Rights::any([
+            Rights::permission("Category::View"),
+            Rights::permission("Admin::View"),
+        ]))
+        .validate(&current_user, &method, None)
+        .await
+    {
+        return format!(
+            "User {}, Does not have permissions needed to view this page please login",
+            current_user.username
+        );
+    }
+
+    format!(
+        "User has Permissions needed. Here are the Users permissions: {:?}",
+        current_user.permissions
+    )
+}
