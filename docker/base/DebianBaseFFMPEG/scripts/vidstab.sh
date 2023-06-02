7d1794097a1537c9dd866503823fa6fae853317f
spootdev <spootdev@gmail.com>
2023-06-01 19:26:19 -0500

    flip to debian for some containers

---
+++ docker/base/DebianBaseFFMPEG/scripts/vidstab.sh
@@ -0,0 +1,6 @@
+# cd /mediakraken/install/
+# git clone https://github.com/georgmartius/vid.stab.git
+cd /mediakraken/install/vid.stab
+cmake .
+make -j`getconf _NPROCESSORS_ONLN`
+make install
--- docker_build/docker_images_list.py
+++ docker_build/docker_images_list.py
@@ -44,13 +44,11 @@ PYPI_MIRROR_PORT = ':8081'
 
 # base OS images to build off of, meaning there is a 'from' in the docker file(s) that use these
 STAGE_ONE_IMAGES = {
-    # 'AlpineBase3142Py3': ('mkbase_alpinepy3', 'alpine:3.14.2', 'base'),
-    # 'AlpineBase3142RustBuilder': ('mkbase_alpine_rust', 'alpine:3.14.2', 'base'),
-    # 'AlpineBase3143Py3': ('mkbase_alpinepy3', 'alpine:3.14.3', 'base'),
     # 'AlpineBase3150Py3': ('mkbase_alpinepy3', 'alpine:3.15.0', 'base'),
     # 'AlpineBase3162Py3': ('mkbase_alpinepy3', 'alpine:3.16.2', 'base'),
     # 'AlpineBase3173Py3': ('mkbase_alpinepy3', 'alpine:3.17.3', 'base'),
     'AlpineBaseFFMPEG': ('mkbase_alpine_ffmpeg', 'alpine:3.17.3', 'base'),
+    'DebianBaseFFMPEG': ('mkbase_debian_ffmpeg', 'debian:bullseye-20230522-slim', 'base'),
     'DebianBase11Py3': ('mkbase_debianpy3', 'python:3.12.0a3-bullseye', 'base'),
     'RustBase': ('mkbase_rust', 'rust:1.69.0', 'base'),
     'RustBaseAlpine': ('mkbase_rust_alpine', 'rust:1.69.0-alpine', 'base'),
