9c784d5c63fda0ada612bd46f0167862171dcc6a
spootdev <spootdev@gmail.com>
2023-07-26 20:13:46 -0500

    xml parse fixes

--- docker/core/mkmetadatamame/src/main.rs
+++ docker/core/mkmetadatamame/src/main.rs
@@ -1,6 +1,5 @@
 use mk_lib_compression;
 use mk_lib_database;
-use mk_lib_file;
 use mk_lib_network;
 use quickxml_to_serde::{xml_string_to_json, Config, JsonArray, JsonType, NullValue};
 use serde_json::json;
@@ -94,7 +93,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         let conf =
                             Config::new_with_custom_values(true, "", "text", NullValue::Ignore)
                                 .add_json_type_override(
-                                    "/machine/@name",
+                                    "/machine/name",
                                     JsonArray::Infer(JsonType::AlwaysString),
                                 )
                                 .add_json_type_override(
@@ -107,9 +106,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
                                 );
                         for line in reader.lines() {
                             let xml_line = &line.unwrap().trim().to_string();
-                            if xml_line.starts_with("\t<machine") == true {
+                            if xml_line.starts_with("<machine") == true {
                                 xml_data = xml_line.to_string();
-                            } else if xml_line.starts_with("\t</machine") == true {
+                            } else if xml_line.starts_with("</machine") == true {
                                 xml_data.push_str(xml_line);
                                 let json_data =
                                     xml_string_to_json(xml_data.to_string(), &conf).unwrap();
@@ -118,7 +117,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                                 mk_lib_database::database_metadata::mk_lib_database_metadata_game::mk_lib_database_metadata_game_insert(
                                         &sqlx_pool,
                                         uuid::Uuid::nil(),
-                                        json_data["machine"]["@name"].to_string(),
+                                        json_data["machine"]["name"].to_string(),
                                         json_data["machine"]["description"].to_string(),
                                         json_data,
                                     )
@@ -169,14 +168,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         let ext = Path::new(&hash_file_path)
                             .extension()
                             .unwrap_or(&std::ffi::OsStr::new("no_extension"));
-                        if ext == ".xml" {
+                        if ext == "xml" {
                             let file = File::open(&hash_file_path).unwrap();
                             let reader = BufReader::new(file);
                             let mut xml_data: String = "".to_owned();
                             let conf =
                                 Config::new_with_custom_values(true, "", "text", NullValue::Ignore)
                                     .add_json_type_override(
-                                        "/software/@name",
+                                        "/software/name",
                                         JsonArray::Infer(JsonType::AlwaysString),
                                     )
                                     .add_json_type_override(
@@ -190,17 +189,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             let mut game_system_uuid = uuid::Uuid::nil();
                             for line in reader.lines() {
                                 let xml_line = &line.unwrap().trim().to_string();
-                                // fetch sytem id from /softwarelist/@name
+                                // fetch sytem id from /softwarelist/name
                                 if xml_line.starts_with("<softwarelist") == true {
+                                    println!("xml_line: {:?}", xml_line);
                                     let system_string_split: Vec<&str> =
                                         xml_line.split("\"").collect();
-                                    game_system_uuid = mk_lib_database::database_metadata::mk_lib_database_metadata_game_system::mk_lib_database_metadata_game_system_guid_by_short_name(&sqlx_pool, &system_string_split[1].to_string()).await.unwrap();
-                                    if game_system_uuid == uuid::Uuid::nil() {
+                                    println!("split: {:?}", system_string_split);
+                                    let system_counter = mk_lib_database::database_metadata::mk_lib_database_metadata_game_system::mk_lib_database_metadata_game_system_game_count_by_short_name(&sqlx_pool, &system_string_split[1].to_string()).await.unwrap();
+                                    if system_counter == 0 {
                                         game_system_uuid = mk_lib_database::database_metadata::mk_lib_database_metadata_game_system::mk_lib_database_metadata_game_system_upsert(&sqlx_pool, system_string_split[1].to_string(), String::new(), json!({})).await.unwrap();
+                                    } else {
+                                        game_system_uuid = mk_lib_database::database_metadata::mk_lib_database_metadata_game_system::mk_lib_database_metadata_game_system_guid_by_short_name(&sqlx_pool, &system_string_split[1].to_string()).await.unwrap();
                                     }
-                                } else if xml_line.starts_with("\t<software") == true {
+                                } else if xml_line.starts_with("<software") == true {
                                     xml_data = xml_line.to_string();
-                                } else if xml_line.starts_with("\t</software") == true {
+                                } else if xml_line.starts_with("</software") == true {
                                     xml_data.push_str(xml_line);
                                     let json_data =
                                         xml_string_to_json(xml_data.to_string(), &conf).unwrap();
@@ -209,7 +212,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                                     mk_lib_database::database_metadata::mk_lib_database_metadata_game::mk_lib_database_metadata_game_insert(
                                         &sqlx_pool,
                                         game_system_uuid,
-                                        json_data["software"]["@name"].to_string(),
+                                        json_data["software"]["name"].to_string(),
                                         json_data["software"]["description"].to_string(),
                                         json_data,
                                     )
@@ -248,31 +251,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     .await
                     .unwrap();
 
-                    let file = File::open(&"/mediakraken/emulation/history.xml")
-                    .unwrap();
+                    let file = File::open(&"/mediakraken/emulation/history.xml").unwrap();
                     let reader = BufReader::new(file);
                     let mut xml_data: String = "".to_owned();
                     let conf = Config::new_with_custom_values(true, "", "text", NullValue::Ignore)
                         .add_json_type_override(
-                            "/entry/software/item/@name",
+                            "/entry/software/item/name",
                             JsonArray::Infer(JsonType::AlwaysString),
                         );
                     for line in reader.lines() {
                         let xml_line = &line.unwrap().trim().to_string();
-                        if xml_line.starts_with("\t<entry") == true {
+                        if xml_line.starts_with("<entry") == true {
                             xml_data = xml_line.to_string();
-                        } else if xml_line.starts_with("\t</entry") == true {
+                        } else if xml_line.starts_with("</entry") == true {
                             xml_data.push_str(xml_line);
                             let json_data =
                                 xml_string_to_json(xml_data.to_string(), &conf).unwrap();
-                            let mut game_system_uuid = mk_lib_database::database_metadata::mk_lib_database_metadata_game_system::mk_lib_database_metadata_game_system_guid_by_short_name(&sqlx_pool, &json_data["entry"]["software"]["item"]["@list"].to_string()).await.unwrap();
+                            let mut game_system_uuid = mk_lib_database::database_metadata::mk_lib_database_metadata_game_system::mk_lib_database_metadata_game_system_guid_by_short_name(&sqlx_pool, &json_data["entry"]["software"]["item"]["list"].to_string()).await.unwrap();
                             if game_system_uuid == uuid::Uuid::nil() {
-                                game_system_uuid = mk_lib_database::database_metadata::mk_lib_database_metadata_game_system::mk_lib_database_metadata_game_system_upsert(&sqlx_pool, json_data["entry"]["software"]["item"]["@list"].to_string(), String::new(), json!({})).await.unwrap();
+                                game_system_uuid = mk_lib_database::database_metadata::mk_lib_database_metadata_game_system::mk_lib_database_metadata_game_system_upsert(&sqlx_pool, json_data["entry"]["software"]["item"]["list"].to_string(), String::new(), json!({})).await.unwrap();
                             }
                             mk_lib_database::database_metadata::mk_lib_database_metadata_game::mk_lib_database_metadata_game_insert(
                                     &sqlx_pool,
                                     game_system_uuid,
-                                    json_data["entry"]["software"]["item"]["@name"].to_string(),
+                                    json_data["entry"]["software"]["item"]["name"].to_string(),
                                     json_data["entry"]["text"].to_string(),
                                     json_data,
                                 )
@@ -283,6 +285,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         }
                     }
                 }
+
+                /*
+                [Category]
+                005=Maze / Shooter Small
+                09825_67907=System / Device
+                100lions=Slot Machine / Video Slot
+                100lionsa=Slot Machine / Video Slot
+                10yard=Sports / Football
+                10yard85=Sports / Football
+                10yardj=Sports / Football
+                110dance=Handheld / Plug n' Play TV Game / Music
+                */
 
                 println!("Here I am 77");
                 // read the category file and create dict/list for it
@@ -308,52 +322,39 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     )
                     .await
                     .unwrap();
-                    let file = File::open(&"/mediakraken/emulation/catver.ini")
-                    .unwrap();
+                    let file = File::open(&"/mediakraken/emulation/catver.ini").unwrap();
                     let reader = BufReader::new(file);
                     let mut category_found = false;
                     for line in reader.lines() {
                         let xml_line = &line.unwrap().trim().to_string();
-                        if xml_line.len() > 1 { // this is to skip blank lines
+                        if xml_line.len() > 1 {
+                            // this is to skip blank lines
                             if category_found == true {
                                 // everything here on out is a game / cat
+                                // TODO
+                                //  result_value = db_connection.db_meta_game_category_by_name(category)
+                                //  if result_value == None:
+                                //        result_value = db_connection.db_meta_game_category_add(category)
+                                //        cat_dictionary[line.strip()] = result_value
+                                //
+                                // TODO
+                                // grab all system null in db as those are mame
+                                //     for sql_row in db_connection.db_media_mame_game_list():
+                                //         db_connection.db_media_game_category_update(cat_dictionary[sql_row["gi_short_name"]],
+                                //                                                     sql_row["gi_id"])
+                                //
+                                // TODO
+                                // grab all the non parent roms that aren't set
+                                //     for sql_row in db_connection.db_media_game_clone_list():
+                                //         for sql_cat_row in db_connection.db_media_game_category_by_name(sql_row["gi_cloneof"]):
+                                //             db_connection.db_media_game_category_update(sql_cat_row["gi_gc_category"],
+                                //                                                         sql_row["gi_id"])
+                                //
                             }
                             if xml_line.starts_with("[Category]") == true {
                                 category_found = true;
                             }
                         }
-
-                        /*
-                        [Category]
-                        005=Maze / Shooter Small
-                        09825_67907=System / Device
-                        100lions=Slot Machine / Video Slot
-                        100lionsa=Slot Machine / Video Slot
-                        10yard=Sports / Football
-                        10yard85=Sports / Football
-                        10yardj=Sports / Football
-                        110dance=Handheld / Plug n' Play TV Game / Music
-                        */
-
-                        // TODO
-                        //  result_value = db_connection.db_meta_game_category_by_name(category)
-                        //  if result_value == None:
-                        //        result_value = db_connection.db_meta_game_category_add(category)
-                        //        cat_dictionary[line.strip()] = result_value
-                        //
-                        // TODO
-                        // grab all system null in db as those are mame
-                        //     for sql_row in db_connection.db_media_mame_game_list():
-                        //         db_connection.db_media_game_category_update(cat_dictionary[sql_row["gi_short_name"]],
-                        //                                                     sql_row["gi_id"])
-                        //
-                        // TODO
-                        // grab all the non parent roms that aren't set
-                        //     for sql_row in db_connection.db_media_game_clone_list():
-                        //         for sql_cat_row in db_connection.db_media_game_category_by_name(sql_row["gi_cloneof"]):
-                        //             db_connection.db_media_game_category_update(sql_cat_row["gi_gc_category"],
-                        //                                                         sql_row["gi_id"])
-                        //
                     }
                 }
 
@@ -376,8 +377,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     )
                     .await
                     .unwrap();
-                    let file = File::open(&"/mediakraken/emulation/messinfo.dat")
-                    .unwrap();
+                    let file = File::open(&"/mediakraken/emulation/messinfo.dat").unwrap();
                     let mut reader = BufReader::new(file);
                     let mut dat_line = String::new();
                     let mut start_system_read = false;
