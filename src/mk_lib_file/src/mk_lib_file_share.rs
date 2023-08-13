ab63785a3d5903071a820c3dfa6881e598fa3cc8
spootdev <spootdev@gmail.com>
2023-08-13 12:06:15 -0500

    try priv

--- src/mk_lib_file/src/mk_lib_file_share.rs
+++ src/mk_lib_file/src/mk_lib_file_share.rs
@@ -33,10 +33,13 @@ pub async fn mk_file_share_mount(
     let output = Command::new("mount")
         .args(&share_options)
         .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .output()
         .unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
     println!("Mount out: {:?}", stdout);
+    let stderr = String::from_utf8(output.stderr).unwrap();
+    println!("Mount err: {:?}", stderr);
     Ok(())
 }
 
