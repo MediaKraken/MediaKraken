997964f7b8b1cea1e3358598e38f4ecc9ce2af3a
spootdev <spootdev@gmail.com>
2024-01-29 18:35:49 -0600

    notes

--- src/mk_lib_database/src/mk_lib_database_copy.rs
+++ src/mk_lib_database/src/mk_lib_database_copy.rs
@@ -18,14 +18,14 @@ pub async fn mk_lib_database_copy(
 ) -> Result<(), sqlx::Error> {
     let mut transaction = sqlx_pool.begin().await?;
     sqlx::query(
-        "CREATE TABLE IF NOT EXISTS mktemp_import5 (temp_type TEXT, temp_key TEXT, temp_revision TEXT, temp_last_modified TIMESTAMP, temp_json TEXT);",
+        "CREATE TABLE IF NOT EXISTS mktemp_import6 (temp_type TEXT, temp_key TEXT, temp_revision TEXT, temp_last_modified TIMESTAMP, temp_json JSONB);",
     )
     .execute(&mut *transaction)
     .await?;
     transaction.commit().await?;
     let mut pg_connection = sqlx_pool.acquire().await.unwrap();
     let mut pg_copy_in = pg_connection
-        .copy_in_raw("copy public.mktemp_import5 (temp_type, temp_key, temp_revision, temp_last_modified, temp_json) from stdin (format text)")
+        .copy_in_raw("copy public.mktemp_import6 (temp_type, temp_key, temp_revision, temp_last_modified, temp_json) from stdin CSV DELIMITER E'\t' QUOTE '\"' ESCAPE '\\'")
         .await
         .unwrap();
     let file = tokio::fs::File::open(copy_file).await?;
@@ -34,4 +34,10 @@ pub async fn mk_lib_database_copy(
     println!("New row inserted: {rows_inserted:}");
     Ok(())
 }
+
+// INSERT ... AS SELECT ... command, where the SELECT is doing conversion to JSONB
 //         .copy_in_raw("copy public.mktemp_import3 (temp_type, temp_key, temp_revision, temp_last_modified, temp_json) from stdin TEXT DELIMITER E'\t' QUOTE '\"' ESCAPE '\\'")
+// ESCAPE '\'  - COPY escape must be a single one-byte character
+// ESCAPE '\\' - First record fails - called `Result::unwrap()` on an `Err` value: Database(PgDatabaseError { severity: Error, code: "22P02", message: "invalid input syntax for type json", detail: Some("Token \"type\" is invalid."), hint: None, position: None, where: Some("JSON data, line 1: {type...\nCOPY mktemp_import6, line 1, column temp_json: \"{type: {key: /type/author}, name: Eileen Schneemilch, key: /authors/OL10000979A, source_records: [bw...\""), schema: None, table: None, column: None, data_type: None, constraint: None, file: Some("jsonfuncs.c"), line: Some(627), routine: Some("json_ereport_error") })
+// ESCAPE '\\\\' - COPY escape must be a single one-byte character
+// QUOTE '\"' - removed - First record fails - called `Result::unwrap()` on an `Err` value: Database(PgDatabaseError { severity: Error, code: "22P02", message: "invalid input syntax for type json", detail: Some("Token \"type\" is invalid."), hint: None, position: None, where: Some("JSON data, line 1: {type...\nCOPY mktemp_import6, line 1, column temp_json: \"{type: {key: /type/author}, name: Eileen Schneemilch, key: /authors/OL10000979A, source_records: [bw...\""), schema: None, table: None, column: None, data_type: None, constraint: None, file: Some("jsonfuncs.c"), line: Some(627), routine: Some("json_ereport_error") })
