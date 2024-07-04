85ab9d4f6fce72eaa1d0f2891256e11fda87f7af
spootdev <spootdev@gmail.com>
2024-07-04 12:40:32 -0500

    popen

--- docker_k8s/mediakraken_setup.py
+++ docker_k8s/mediakraken_setup.py
@@ -3,32 +3,50 @@ import shlex
 import subprocess
 from base64 import b64encode
 
-if not os.path.isfile('./mkstack-db_password.txt'):
-    file_handle = open('./mkstack-db_password.txt', 'w+')
+if not os.path.isfile('./mkstack-db-password.txt'):
+    file_handle = open('./mkstack-db-password.txt', 'w+')
     random_key = b64encode(os.urandom(32)).decode('utf-8')
     file_handle.write(random_key.replace(
         '"', '').replace("'", '').replace("%", ''))
     file_handle.close()
-    install_pid = subprocess.call(shlex.split('kubectl create secret generic db-password --from-file=./mkstack-db_password.txt --namespace=mediakraken'),
+    install_pid = subprocess.Popen(shlex.split('kubectl create secret generic db-password --from-file=./mkstack-db-password.txt --namespace=mediakraken'),
                                   stdout=subprocess.PIPE, shell=False)
+    while True:
+        line = install_pid.stdout.readline()
+        if not line:
+            break
+        print(line.rstrip(), flush=True)
+    install_pid.wait()
 
-if not os.path.isfile('./mkstack-secure_key.txt'):
-    file_handle = open('./mkstack-secure_key.txt', 'w+')
+if not os.path.isfile('./mkstack-secure-key.txt'):
+    file_handle = open('./mkstack-secure-key.txt', 'w+')
     random_key = b64encode(os.urandom(32)).decode('utf-8')
     file_handle.write(random_key.replace(
         '"', '').replace("'", '').replace("%", ''))
     file_handle.close()
-    install_pid = subprocess.call(shlex.split('kubectl create secret generic secure-key --from-file=./mkstack-secure_key.txt --namespace=mediakraken'),
+    install_pid = subprocess.Popen(shlex.split('kubectl create secret generic secure-key --from-file=./mkstack-secure-key.txt --namespace=mediakraken'),
                                   stdout=subprocess.PIPE, shell=False)
+    while True:
+        line = install_pid.stdout.readline()
+        if not line:
+            break
+        print(line.rstrip(), flush=True)
+    install_pid.wait()
 
-if not os.path.isfile('./mkstack-csrf_key.txt'):
-    file_handle = open('./mkstack-csrf_key.txt', 'w+')
+if not os.path.isfile('./mkstack-csrf-key.txt'):
+    file_handle = open('./mkstack-csrf-key.txt', 'w+')
     random_key = b64encode(os.urandom(32)).decode('utf-8')
     file_handle.write(random_key.replace(
         '"', '').replace("'", '').replace("%", ''))
     file_handle.close()
-    install_pid = subprocess.call(shlex.split('kubectl create secret generic csrf-key --from-file=./mkstack-csrf_key.txt --namespace=mediakraken'),
+    install_pid = subprocess.Popen(shlex.split('kubectl create secret generic csrf-key --from-file=./mkstack-csrf-key.txt --namespace=mediakraken'),
                                   stdout=subprocess.PIPE, shell=False)
+    while True:
+        line = install_pid.stdout.readline()
+        if not line:
+            break
+        print(line.rstrip(), flush=True)
+    install_pid.wait()
 
 if not os.path.isfile('./mkstack-nut.txt'):
     file_handle = open('./mkstack-nut.txt', 'w+')
@@ -36,7 +54,13 @@ if not os.path.isfile('./mkstack-nut.txt'):
     file_handle.write(random_key.replace(
         '"', '').replace("'", '').replace("%", ''))
     file_handle.close()
-    install_pid = subprocess.call(shlex.split('kubectl create secret generic nut-password --from-file=./mkstack-nut.txt --namespace=mediakraken'),
+    install_pid = subprocess.Popen(shlex.split('kubectl create secret generic nut-password --from-file=./mkstack-nut.txt --namespace=mediakraken'),
                                   stdout=subprocess.PIPE, shell=False)
+    while True:
+        line = install_pid.stdout.readline()
+        if not line:
+            break
+        print(line.rstrip(), flush=True)
+    install_pid.wait()
 
 # TODO when production, remove the secret files
