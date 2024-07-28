e572e7dabc5dd06e4182cea552d5d7b28889365c
spootdev <spootdev@gmail.com>
2024-07-28 09:57:51 -0500

    install opentofu

--- docker_k8s/opentofu/proxmox/main.tf
+++ docker_k8s/opentofu/proxmox/main.tf
@@ -1,15 +1,24 @@
 provider "proxmox" {
- pm_api_url   = "https://your-proxmox-host:8006/api2/json"
- pm_user      = "blog_example"
- pm_password  = "your-api-key"
+ pm_api_url   = "https://192.168.1.11:8006/api2/json"
+ pm_user      = "metaman"
+ pm_password  = "12d03d06-d66e-4950-abbf-41a2757cfed6"
  pm_tls_insecure = true
 }
 
-resource "proxmox_vm_qemu" "my_vm" {
- name       = "my-vm"
- target_node = "pve"
+resource "proxmox_vm_qemu" "mkcontrol1" {
+ name       = "mkcontrol1"
+ target_node = "pvezfs"
  clone      = "ubuntu-template"
  storage    = "local-lvm"
- cores      = 2
- memory     = 2048
+ cores      = 4
+ memory     = 8192
+}
+
+resource "proxmox_vm_qemu" "mkcontrol2" {
+ name       = "mkcontrol2"
+ target_node = "pvezfs"
+ clone      = "ubuntu-template"
+ storage    = "local-lvm"
+ cores      = 4
+ memory     = 8192
 }
