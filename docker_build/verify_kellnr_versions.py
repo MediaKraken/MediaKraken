6eb8b86de1f92f69def952d627cf2dd744be0885
spootdev <spootdev@gmail.com>
2024-10-30 20:52:58 -0500

    have kellnr auto update versions

--- docker_build/verify_kellnr_versions.py
+++ docker_build/verify_kellnr_versions.py
@@ -1,5 +1,6 @@
 # find all current kellnr versions in CODE
 from glob import glob
+import subprocess
 
 # grab base versions of my libs
 list_of_crates = {}
@@ -11,6 +12,7 @@ for filename in glob('../src/mk_lib_*/Cargo.toml', recursive=True):
 print("Cargos: ", list_of_crates)
 
 # loop through librarys
+fix_libs = False
 for filename in glob('../src/mk_lib*/Cargo.toml', recursive=True):
     f=open(filename)
     lines=f.readlines()
@@ -21,15 +23,21 @@ for filename in glob('../src/mk_lib*/Cargo.toml', recursive=True):
             version = line.split("\"")[1]
             if list_of_crates[lib] != version: 
                 print(filename, list_of_crates[lib], lib, version)
+                fix_libs = True
 
-# loop through docker images
-for filename in glob('../docker/core/mk*/Cargo.toml', recursive=True):
-    f=open(filename)
-    lines=f.readlines()
-    f.close()
-    for line in lines:
-        if line.find("mk_lib_") == 0:
-            lib = line.split(" ")[0]
-            version = line.split("\"")[1]
-            if list_of_crates[lib] != version: 
-                print(filename, list_of_crates[lib], lib, version)
+# might as well fix ALL libs first
+if fix_libs is False:
+    # loop through docker images
+    for filename in glob('../docker/core/mk*/Cargo.toml', recursive=True):
+        f=open(filename)
+        lines=f.readlines()
+        f.close()
+        for line in lines:
+            if line.find("mk_lib_") == 0:
+                lib = line.split(" ")[0]
+                version = line.split("\"")[1]
+                if list_of_crates[lib] != version: 
+                    print(filename, list_of_crates[lib], lib, version)
+                    subprocess.Popen("sed -i 's/" + lib + " = { version = \"" + version + "\"/" 
+                                    + lib + " = { version = \"" + list_of_crates[lib] + "\""
+                                    + "/g' " + filename, shell=True)
