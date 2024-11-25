a7e548e3a4f182d8a27c2a6bf269be963441cc8d
spootdev <spootdev@gmail.com>
2024-11-24 19:18:42 -0600

    streamline kubespray init

---
+++ docker_build/kubespray_update.py
@@ -0,0 +1,13 @@
+import os
+
+# copy kubespray inventory to mk dirs
+os.system("cp -rf ../../kubespray/inventory/sample ../../kubespray/inventory/mkcluster")
+os.system("cp -rf ../../kubespray/inventory/sample ../../kubespray/inventory/mkclusterdev")
+
+# SED the cluster names
+os.system("sed -i -e 's/cluster_name: cluster.local/cluster_name: mkcluster.local/g' ../../kubespray/inventory/mkcluster/group_vars/k8s_cluster/k8s-cluster.yml")
+os.system("sed -i -e 's/cluster_name: cluster.local/cluster_name: mkclusterdev.local/g' ../../kubespray/inventory/mkclusterdev/group_vars/k8s_cluster/k8s-cluster.yml")
+
+# copy the inventory to mk dirs
+os.system("cp -f ../k8s/cluster/inventory.ini ../../kubespray/inventory/mkcluster/.")
+os.system("cp -f ../k8s_devenv/cluster/inventory.ini ../../kubespray/inventory/mkclusterdev/.")
