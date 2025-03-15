616e351da6667b427dd9fc8ea1dc6d79e36f0dae
spootdev <spootdev@gmail.com>
2025-03-15 15:07:30 -0500

    core bumps

--- docker_build/verify_kellnr_versions.py
+++ docker_build/verify_kellnr_versions.py
@@ -1,43 +1,43 @@
-# find all current kellnr versions in CODE
 from glob import glob
 import subprocess
 
-# grab base versions of my libs
+# find all current kellnr versions and bump level
 list_of_crates = {}
 for filename in glob('../src/mk_lib_*/Cargo.toml', recursive=True):
     f=open(filename)
     lines=f.readlines()
-    list_of_crates[lines[1].strip().split(" ")[2].replace("\"", "")] = lines[2].strip().split(" ")[2].replace("\"", "")
+    old_version = lines[2].strip().split(" ")[2].replace("\"", "")
+    crate_version = lines[2].strip().split(" ")[2].replace("\"", "").rsplit(".", 1)
+    new_version = crate_version[0] + "." + str(int(crate_version[1]) + 1)
+    list_of_crates[lines[1].strip().split(" ")[2].replace("\"", "")] = new_version
     f.close()
-print("Cargos: ", list_of_crates)
+    subprocess.Popen("sed -i 's/version = \"" + old_version + "\"version = \"" + list_of_crates[lib] + "\""
+        + "/g' " + filename, shell=True)
+print("My Kellnr Cargos: ", list_of_crates)
 
-# loop through librarys
-fix_libs = False
-for filename in glob('../src/mk_lib*/Cargo.toml', recursive=True):
-    f=open(filename)
-    lines=f.readlines()
-    f.close()
-    for line in lines:
-        if line.find("mk_lib_") == 0:
-            lib = line.split(" ")[0]
-            version = line.split("\"")[1]
-            if list_of_crates[lib] != version: 
-                print(filename, list_of_crates[lib], lib, version)
-                fix_libs = True
+# # loop through librarys
+# for filename in glob('../src/mk_lib*/Cargo.toml', recursive=True):
+#     f=open(filename)
+#     lines=f.readlines()
+#     f.close()
+#     for line in lines:
+#         if line.find("mk_lib_") == 0:
+#             lib = line.split(" ")[0]
+#             version = line.split("\"")[1]
+#             if list_of_crates[lib] != version: 
+#                 print(filename, list_of_crates[lib], lib, version)
 
-# might as well fix ALL libs first
-if fix_libs is False:
-    # loop through docker images
-    for filename in glob('../docker/core/mk*/Cargo.toml', recursive=True):
-        f=open(filename)
-        lines=f.readlines()
-        f.close()
-        for line in lines:
-            if line.find("mk_lib_") == 0:
-                lib = line.split(" ")[0]
-                version = line.split("\"")[1]
-                if list_of_crates[lib] != version: 
-                    print(filename, list_of_crates[lib], lib, version)
-                    subprocess.Popen("sed -i 's/" + lib + " = { version = \"" + version + "\"/" 
-                                    + lib + " = { version = \"" + list_of_crates[lib] + "\""
-                                    + "/g' " + filename, shell=True)
+# # loop through docker images
+# for filename in glob('../docker/core/mk*/Cargo.toml', recursive=True):
+#     f=open(filename)
+#     lines=f.readlines()
+#     f.close()
+#     for line in lines:
+#         if line.find("mk_lib_") == 0:
+#             lib = line.split(" ")[0]
+#             version = line.split("\"")[1]
+#             if list_of_crates[lib] != version: 
+#                 print(filename, list_of_crates[lib], lib, version)
+#                 subprocess.Popen("sed -i 's/" + lib + " = { version = \"" + version + "\"/" 
+#                                 + lib + " = { version = \"" + list_of_crates[lib] + "\""
+#                                 + "/g' " + filename, shell=True)
