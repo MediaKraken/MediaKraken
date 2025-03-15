f3bca2f8beca5100b39bf1c0897eb970eb7b88a2
spootdev <spootdev@gmail.com>
2025-03-15 18:07:06 -0500

    auto bump kellnr libs

--- docker_build/verify_kellnr_versions.py
+++ docker_build/verify_kellnr_versions.py
@@ -1,6 +1,19 @@
 from glob import glob
 import subprocess
 
+def sed_file(filename):
+    f=open(filename)
+    lines=f.readlines()
+    f.close()
+    for line in lines:
+        if line.find("mk_lib_") == 0:
+            lib = line.split(" ")[0]
+            version = line.split("\"")[1]
+            if list_of_crates[lib] != version: 
+                subprocess.Popen("sed -i 's/" + lib + " = { version = \"" + version + "\"/" 
+                                + lib + " = { version = \"" + list_of_crates[lib] + "\""
+                                + "/g' " + filename, shell=True)
+
 # find all current kellnr versions and bump level
 list_of_crates = {}
 for filename in glob('../src/mk_lib_*/Cargo.toml', recursive=True):
@@ -11,33 +24,14 @@ for filename in glob('../src/mk_lib_*/Cargo.toml', recursive=True):
     new_version = crate_version[0] + "." + str(int(crate_version[1]) + 1)
     list_of_crates[lines[1].strip().split(" ")[2].replace("\"", "")] = new_version
     f.close()
-    subprocess.Popen("sed -i 's/version = \"" + old_version + "\"version = \"" + list_of_crates[lib] + "\""
-        + "/g' " + filename, shell=True)
+    command_to_run = "sed -i '3 s/version = \"" + old_version + "\"/version = \"" + new_version + "\"/g' " + filename
+    subprocess.Popen(command_to_run, shell=True)
 print("My Kellnr Cargos: ", list_of_crates)
 
-# # loop through librarys
-# for filename in glob('../src/mk_lib*/Cargo.toml', recursive=True):
-#     f=open(filename)
-#     lines=f.readlines()
-#     f.close()
-#     for line in lines:
-#         if line.find("mk_lib_") == 0:
-#             lib = line.split(" ")[0]
-#             version = line.split("\"")[1]
-#             if list_of_crates[lib] != version: 
-#                 print(filename, list_of_crates[lib], lib, version)
+# loop through librarys
+for filename in glob('../src/mk_lib*/Cargo.toml', recursive=True):
+    sed_file(filename)
 
-# # loop through docker images
-# for filename in glob('../docker/core/mk*/Cargo.toml', recursive=True):
-#     f=open(filename)
-#     lines=f.readlines()
-#     f.close()
-#     for line in lines:
-#         if line.find("mk_lib_") == 0:
-#             lib = line.split(" ")[0]
-#             version = line.split("\"")[1]
-#             if list_of_crates[lib] != version: 
-#                 print(filename, list_of_crates[lib], lib, version)
-#                 subprocess.Popen("sed -i 's/" + lib + " = { version = \"" + version + "\"/" 
-#                                 + lib + " = { version = \"" + list_of_crates[lib] + "\""
-#                                 + "/g' " + filename, shell=True)
+# loop through docker images
+for filename in glob('../docker/core/mk*/Cargo.toml', recursive=True):
+    sed_file(filename)
