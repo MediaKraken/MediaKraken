df0d84a1ef0e05f3378d440ac370ef58cb3ba3fd
spootdev <spootdev@gmail.com>
2025-03-15 19:11:47 -0500

    kellnr fixes for autobump

--- docker_build/build_and_deploy_to_kellnr.py
+++ docker_build/build_and_deploy_to_kellnr.py
@@ -1,20 +1,29 @@
 import argparse
 import os
 import subprocess
+import network_email
+import shlex
+
+try:
+    from dotenv import load_dotenv
+except ModuleNotFoundError:
+    install_pid = subprocess.Popen(shlex.split('apt-get install python3-dotenv -y'),
+                                   stdout=subprocess.PIPE, shell=False)
+    install_pid.wait()
+    from dotenv import load_dotenv
 
 CWD_HOME_DIRECTORY = os.getcwd().rsplit('MediaKraken', 1)[0]
 os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken/src'))
 
 parser = argparse.ArgumentParser(
     description='This program builds and deploys MediaKraken Libraries')
-parser.add_argument('-e', '--email', required=False,
-                    help='Send results email', action="store_true")
 # set args.image variable if entered - ex. mkwebaxum
 parser.add_argument('-l', '--library', metavar='library', required=False,
                     help='Library to build')
 args = parser.parse_args()
 
-# TODO check for failures?
+# load .env stats
+load_dotenv()
 
 # publish to kellnr
 libs_to_publish = ["mk_lib_common",
@@ -38,9 +47,31 @@ for lib_name in libs_to_publish:
     print(lib_name)
     os.chdir(run_path + "/../src/" + lib_name)
     # build the library
-    pid_proc = subprocess.Popen(['cargo', 'build', '-p', lib_name])
-    pid_proc.wait()
-    # TODO send to mailhog the results, don't publish if failed
-    # publish the libary
-    pid_proc = subprocess.Popen(["cargo", "publish", "--registry", "kellnr", "-p", lib_name, "--allow-dirty", "--token=3dPvCZSd1V8xKpiCGqpXCBbkj4Jtyyqc"])
-    pid_proc.wait()
+    pid_build_proc = subprocess.Popen(['cargo', 'build', '-p', lib_name],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      shell=False)
+    (out, err) = pid_build_proc.communicate()
+    email_body = err.decode("utf-8")
+    subject_text = ' FAILED'
+    if email_body.find('Finished') != -1 and email_body.find(' in') != -1:
+        subject_text = ' SUCCESS'
+        # publish the libary
+        pid_push_proc = subprocess.Popen(["cargo", "publish", "--registry", "kellnr", "-p",
+                                            lib_name, "--allow-dirty", "--token=3dPvCZSd1V8xKpiCGqpXCBbkj4Jtyyqc"],
+                stdout=subprocess.PIPE, shell=False)
+        while True:
+            line = pid_push_proc.stdout.readline()
+            if not line:
+                break
+            print(line.rstrip(), flush=True)
+        pid_push_proc.wait()
+    # send success/fail email
+    network_email.com_net_send_email(os.environ['MAILUSER'],
+                                        os.environ['MAILPASS'],
+                                        os.environ['MAILUSER'],
+                                        'Build ' + lib_name + ' crate: '
+                                        + subject_text,
+                                        email_body,
+                                        smtp_server=os.environ['MAILSERVER'],
+                                        smtp_port=os.environ['MAILPORT'])
