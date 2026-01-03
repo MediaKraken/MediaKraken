c329672a690d3557d2fa9e0c23bb15c45e3bc5cc
spootdev <spootdev@gmail.com>
2026-01-03 16:11:28 -0600

    finish remove of tiaga

--- k8s_devenv/environment/main.tf
+++ k8s_devenv/environment/main.tf
@@ -97,15 +97,6 @@ resource "terraform_data" "ntfy" {
   ]
 }
 
-resource "terraform_data" "taiga" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -b -v -u ${var.vm_user} -i inventory.ini playbooks/taiga.yml"
-  }
-  depends_on = [
-    terraform_data.ntfy
-  ]
-}
-
 resource "terraform_data" "gitea" {
   provisioner "local-exec" {
     command = "ansible-playbook -b -v -u ${var.vm_user} -i inventory.ini playbooks/gitea.yml"
