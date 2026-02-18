a1f2ea9b63770088c042a110840700bd039f768e
SpootDev <spootdev@gmail.com>
2026-02-18 13:24:11 -0600

    notes

--- docker/core/mksharescanner/src/main.rs
+++ docker/core/mksharescanner/src/main.rs
@@ -42,6 +42,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     .await
                     .unwrap();
                 for share_info in share_vec.iter() {
+                    // TODO make an upsert
                     if mk_lib_database::mk_lib_database_network_share::mk_lib_database_network_share_exists(&sqlx_pool_ro,
                             share_info.mm_share_ip,
                             share_info.mm_share_path.clone(),).await.unwrap() == false {
