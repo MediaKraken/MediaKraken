12a85d777e25b527ded41e908b0d4e99b339b996
spootdev <spootdev@gmail.com>
2026-02-25 19:38:36 -0600

    hash fixes

--- src/mk_lib_hash/src/mk_lib_hash_blake3.rs
+++ src/mk_lib_hash/src/mk_lib_hash_blake3.rs
@@ -5,7 +5,10 @@ use std::error::Error;
 
 pub async fn mk_file_hash_blake3(file_to_read: &str) -> Result<String, Box<dyn Error>> {
     let mut hasher = blake3::Hasher::new();
-    read_file_chunks(file_to_read, |chunk| hasher.update(chunk)).await?;
+    read_file_chunks(file_to_read, |chunk| {
+        hasher.update(chunk);
+    })
+    .await?;
     let checksum = hasher.finalize();
     Ok(format!("{}", checksum))
 }
