949bb45fbaba77f46bf70adf6d5b2aa00d87e8c6
spootdev <spootdev@gmail.com>
2026-03-15 18:12:15 -0500

    lang falback

--- src/mk_lib_common/src/mk_lib_common_internationalization.rs
+++ src/mk_lib_common/src/mk_lib_common_internationalization.rs
@@ -1,13 +1,10 @@
 // https://github.com/bcmyers/num-format
 
-use num_format::SystemLocale;
-use num_format::WriteFormatted;
+use num_format::{Locale, SystemLocale, ToFormattedString};
 
-pub fn mk_lib_common_internationalization_number_format(
-    number_for_format: i64,
-) -> Result<String, std::io::Error> {
-    let locale = SystemLocale::default().unwrap();
-    let mut writer = String::new();
-    let _res = writer.write_formatted(&number_for_format, &locale);
-    Ok(writer)
+pub fn mk_lib_common_internationalization_number_format(number_for_format: i64) -> String {
+    match SystemLocale::default() {
+        Ok(locale) => number_for_format.to_formatted_string(&locale),
+        Err(_) => number_for_format.to_formatted_string(&Locale::en),
+    }
 }
