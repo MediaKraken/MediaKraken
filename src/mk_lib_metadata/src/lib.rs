6c3af000445f1fe349ada1fca39789d84d9dbe80
SpootDev <SpootDev@users.noreply.github.com>
2026-03-16 18:23:46 -0500

    Add eBay UPC search metadata provider helpers

--- src/mk_lib_metadata/src/lib.rs
+++ src/mk_lib_metadata/src/lib.rs
@@ -25,6 +25,7 @@ pub mod metadata_provider {
     pub mod coverartarchive;
     #[cfg(feature = "discid")]
     pub mod discid;
+    pub mod ebay;
     pub mod flickr;
     pub mod giant_bomb;
     pub mod goupc;
