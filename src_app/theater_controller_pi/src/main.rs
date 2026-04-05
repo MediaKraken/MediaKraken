b21d6104b4afa3d99fd1b0c042bad952a9fbd624
spootdev <spootdev@gmail.com>
2026-04-05 17:21:22 -0500

    bump and fixes

--- src_app/theater_controller_pi/src/main.rs
+++ src_app/theater_controller_pi/src/main.rs
@@ -1,157 +1,154 @@
 use crossbeam_channel::unbounded;
-use fltk::{app, button::Button, image::SharedImage, prelude::*, window::Window};
-use mk_lib_network;
-use std::error::Error;
-use fltk_theme::widget_schemes::fluent::colors::*;
-use fltk_theme::widget_schemes::fluent::frames::*;
+use fltk::{app, button::Button, prelude::*, window::Window};
 use fltk_theme::{SchemeType, WidgetScheme};
+use std::error::Error;
+
+#[derive(Debug, Clone, Copy)]
+enum UiMsg {
+    ShowSettings,
+    ShowMenu,
+}
+
+fn make_image_button(
+    x: i32,
+    y: i32,
+    w: i32,
+    h: i32,
+    png_bytes: &[u8],
+) -> Result<Button, Box<dyn Error>> {
+    let mut button = Button::new(x, y, w, h, None);
+    let mut image = fltk::image::PngImage::from_data(png_bytes)?;
+    image.scale(w, h, true, true);
+    button.set_image(Some(image));
+    Ok(button)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // load images
-    let bytes_image_rectangle = include_bytes!("../../../docker/core/mkwebaxum/static/image/rectangles_black.png");
-    let bytes_image_new = include_bytes!("../../../docker/core/mkwebaxum/static/image/new.png");
-    let bytes_image_movie_ticket = include_bytes!("../../../docker/core/mkwebaxum/static/image/movie_ticket.png");
-    let bytes_image_television = include_bytes!("../../../docker/core/mkwebaxum/static/image/television.png");
-    let bytes_image_vid_game = include_bytes!("../../../docker/core/mkwebaxum/static/image/vid_game.png");
-    let bytes_image_theater = include_bytes!("../../../docker/core/mkwebaxum/static/image/theater.png");
-    let bytes_image_headphone = include_bytes!("../../../docker/core/mkwebaxum/static/image/headphone.png");
-    let bytes_image_television_live = include_bytes!("../../../docker/core/mkwebaxum/static/image/television_live.png");
-    let bytes_image_vid_camera = include_bytes!("../../../docker/core/mkwebaxum/static/image/vid_camera.png");
-    let bytes_image_earth = include_bytes!("../../../docker/core/mkwebaxum/static/image/earth.png");
-    let bytes_image_listening_music_video_clip_with_auricular = include_bytes!("../../../docker/core/mkwebaxum/static/image/listening-music-video-clip-with-auricular.png");
-    let bytes_image_photo = include_bytes!("../../../docker/core/mkwebaxum/static/image/photo.png");
-    let bytes_image_radio = include_bytes!("../../../docker/core/mkwebaxum/static/image/radio.png");
-    let bytes_image_books = include_bytes!("../../../docker/core/mkwebaxum/static/image/books.png");
-    let bytes_image_settings = include_bytes!("../../../docker/core/mkwebaxum/static/image/settings.png");
-    let bytes_image_return = include_bytes!("../../../docker/core/mkwebaxum/static/image/navigation/return.png");
+    // Load images
+    let bytes_image_rectangle =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/rectangles_black.png");
+    let bytes_image_new =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/new.png");
+    let bytes_image_movie_ticket =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/movie_ticket.png");
+    let bytes_image_television =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/television.png");
+    let bytes_image_vid_game =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/vid_game.png");
+    let bytes_image_theater =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/theater.png");
+    let bytes_image_headphone =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/headphone.png");
+    let bytes_image_television_live =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/television_live.png");
+    let bytes_image_vid_camera =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/vid_camera.png");
+    let bytes_image_earth =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/earth.png");
+    let bytes_image_music_video = include_bytes!(
+        "../../../docker/core/mkwebaxum/static/image/listening-music-video-clip-with-auricular.png"
+    );
+    let bytes_image_photo =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/photo.png");
+    let bytes_image_radio =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/radio.png");
+    let bytes_image_books =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/books.png");
+    let bytes_image_settings =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/settings.png");
+    let bytes_image_return =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/navigation/return.png");
 
-    let _server_list = mk_lib_network::mk_lib_network_mediakraken::mk_lib_network_find_mediakraken_server();
+    let _server_list =
+        mk_lib_network::mk_lib_network_mediakraken::mk_lib_network_find_mediakraken_server();
+
     let app = app::App::default().with_scheme(app::Scheme::Gleam);
-    let mut window_main = Window::default().with_size(800, 480); // pi 7" screen default
-    let mut window_menu = Window::default().with_size(800, 480);
-    // window_menu - left side buttons
-    let mut button_in_progress = Button::new(0, 0, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_rectangle)?;
-    image.scale(133, 96, true, true);
-    button_in_progress.set_image(Some(image));
-    let mut button_new = Button::new(0, 96, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_new)?;
-    image.scale(133, 96, true, true);
-    button_new.set_image(Some(image));
-    let mut button_movie = Button::new(0, 192, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_movie_ticket)?;
-    image.scale(133, 96, true, true);
-    button_movie.set_image(Some(image));
-    let mut button_tv = Button::new(0, 288, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_television)?;
-    image.scale(133, 96, true, true);
-    button_tv.set_image(Some(image));
-    let mut button_game = Button::new(0, 384, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_vid_game)?;
-    image.scale(133, 96, true, true);
-    button_game.set_image(Some(image));
-    // window_menu - top middle button
-    let mut button_demo = Button::new(133, 0, 532, 384, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_theater)?;
-    image.scale(532, 384, true, true);
-    button_demo.set_image(Some(image));
-    // window_menu - bottom middle buttons
-    let mut button_music = Button::new(133, 384, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_headphone)?;
-    image.scale(133, 96, true, true);
-    button_music.set_image(Some(image));
-    let mut button_live_tv = Button::new(266, 384, 133, 96, None);
-    let mut image =
-    fltk::image::PngImage::from_data(bytes_image_television_live)?;
-    image.scale(133, 96, true, true);
-    button_live_tv.set_image(Some(image));
-    let mut button_home_video = Button::new(399, 384, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_vid_camera)?;
-    image.scale(133, 96, true, true);
-    button_home_video.set_image(Some(image));
-    let mut button_internet = Button::new(532, 384, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_earth)?;
-    image.scale(133, 96, true, true);
-    button_internet.set_image(Some(image));
-    // window_menu - right side buttons
-    let mut button_music_video = Button::new(666, 0, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_listening_music_video_clip_with_auricular)?;
-    image.scale(133, 96, true, true);
-    button_music_video.set_image(Some(image));
-    let mut button_pictures = Button::new(666, 96, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_photo)?;
-    image.scale(133, 96, true, true);
-    button_pictures.set_image(Some(image));
-    let mut button_radio = Button::new(666, 192, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_radio)?;
-    image.scale(133, 96, true, true);
-    button_radio.set_image(Some(image));
-    let mut button_books = Button::new(666, 288, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_books)?;
-    image.scale(133, 96, true, true);
-    button_books.set_image(Some(image));
-    let mut button_settings = Button::new(666, 384, 133, 96, None);
-    let mut image = fltk::image::PngImage::from_data(bytes_image_settings)?;
-    image.scale(133, 96, true, true);
-    button_settings.set_image(Some(image));
-    window_menu.end();
-    window_menu.make_resizable(true);
-    window_menu.fullscreen(true);
 
     let theme = WidgetScheme::new(SchemeType::Fluent);
     theme.apply();
-    
-    let mut window_settings = Window::default().with_size(800, 480);
-    let mut button_settings_back = Button::new(666, 384, 133, 96, None);
 
-    let mut image = fltk::image::PngImage::from_data(bytes_image_return)?;
-    image.scale(133, 96, true, true);
-    button_settings_back.set_image(Some(image));
+    let (sender, receiver) = unbounded::<UiMsg>();
+
+    // Menu window
+    let mut window_menu = Window::default().with_size(800, 480);
+
+    let _button_in_progress =
+        make_image_button(0, 0, 133, 96, bytes_image_rectangle)?;
+    let _button_new =
+        make_image_button(0, 96, 133, 96, bytes_image_new)?;
+    let _button_movie =
+        make_image_button(0, 192, 133, 96, bytes_image_movie_ticket)?;
+    let _button_tv =
+        make_image_button(0, 288, 133, 96, bytes_image_television)?;
+    let _button_game =
+        make_image_button(0, 384, 133, 96, bytes_image_vid_game)?;
+
+    let _button_demo =
+        make_image_button(133, 0, 532, 384, bytes_image_theater)?;
+
+    let _button_music =
+        make_image_button(133, 384, 133, 96, bytes_image_headphone)?;
+    let _button_live_tv =
+        make_image_button(266, 384, 133, 96, bytes_image_television_live)?;
+    let _button_home_video =
+        make_image_button(399, 384, 133, 96, bytes_image_vid_camera)?;
+    let _button_internet =
+        make_image_button(532, 384, 133, 96, bytes_image_earth)?;
+
+    let _button_music_video =
+        make_image_button(666, 0, 133, 96, bytes_image_music_video)?;
+    let _button_pictures =
+        make_image_button(666, 96, 133, 96, bytes_image_photo)?;
+    let _button_radio =
+        make_image_button(666, 192, 133, 96, bytes_image_radio)?;
+    let _button_books =
+        make_image_button(666, 288, 133, 96, bytes_image_books)?;
+    let mut button_settings =
+        make_image_button(666, 384, 133, 96, bytes_image_settings)?;
+
+    window_menu.end();
+    window_menu.make_resizable(true);
+    window_menu.fullscreen(true);
+    window_menu.show();
+
+    // Settings window
+    let mut window_settings = Window::default().with_size(800, 480);
+    let mut button_settings_back =
+        make_image_button(666, 384, 133, 96, bytes_image_return)?;
+
     window_settings.end();
     window_settings.make_resizable(true);
     window_settings.fullscreen(true);
     window_settings.hide();
 
-    let mut window_media = Window::default().with_size(800, 480);
+    {
+        let sender = sender.clone();
+        button_settings.set_callback(move |_| {
+            let _ = sender.send(UiMsg::ShowSettings);
+        });
+    }
 
-    window_media.end();
-    window_media.make_resizable(true);
-    window_media.fullscreen(true);
-    window_media.hide();
+    {
+        let sender = sender.clone();
+        button_settings_back.set_callback(move |_| {
+            let _ = sender.send(UiMsg::ShowMenu);
+        });
+    }
 
-    // close up the add to main window and setup current page
-    window_main.end();
-    window_main.make_resizable(true);
-    window_main.fullscreen(true);
-    window_main.show();
-    window_menu.make_current();
-
-    // setup the event
-    let (s, r) = unbounded::<i32>();
-
-    // main buttons
-    button_settings.set_callback(move |_| {
-        s.send(1).unwrap();
-    });
-
-    // media list page
-
-    // settings page
-    // button_settings_back.set_callback(move |bb| {
-    //     window_menu.show();
-    //     window_settings.hide();
-    // });
-    // button_settings_back.set_callback(move |_| {
-    //     s.send(2).unwrap();
-    // });
-
-    // app.run()?;
     while app.wait() {
-        if let Ok(_msg) = r.try_recv() {
-            //button_settings.set_label(&format!("{}", msg));
-            window_menu.hide();
-            window_settings.show();
+        if let Ok(msg) = receiver.try_recv() {
+            match msg {
+                UiMsg::ShowSettings => {
+                    window_menu.hide();
+                    window_settings.show();
+                }
+                UiMsg::ShowMenu => {
+                    window_settings.hide();
+                    window_menu.show();
+                }
+            }
         }
     }
+
     Ok(())
 }
--- src_app/theater_full/src/main.rs
+++ src_app/theater_full/src/main.rs
@@ -15,11 +15,25 @@ fn set_button_image(
     Ok(())
 }
 
+fn make_image_button(
+    x: i32,
+    y: i32,
+    w: i32,
+    h: i32,
+    label: &str,
+    image_bytes: &[u8],
+) -> Result<Button, Box<dyn Error>> {
+    let mut button = Button::new(x, y, w, h, label);
+    set_button_image(&mut button, image_bytes, w, h)?;
+    Ok(button)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // load images
     let bytes_image_rectangle =
         include_bytes!("../../../docker/core/mkwebaxum/static/image/rectangles_black.png");
-    let bytes_image_new = include_bytes!("../../../docker/core/mkwebaxum/static/image/new.png");
+    let bytes_image_new =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/new.png");
     let bytes_image_movie_ticket =
         include_bytes!("../../../docker/core/mkwebaxum/static/image/movie_ticket.png");
     let bytes_image_television =
@@ -34,13 +48,17 @@ fn main() -> Result<(), Box<dyn Error>> {
         include_bytes!("../../../docker/core/mkwebaxum/static/image/television_live.png");
     let bytes_image_vid_camera =
         include_bytes!("../../../docker/core/mkwebaxum/static/image/vid_camera.png");
-    let bytes_image_earth = include_bytes!("../../../docker/core/mkwebaxum/static/image/earth.png");
-    let bytes_image_listening_music_video_clip_with_auricular = include_bytes!(
+    let bytes_image_earth =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/earth.png");
+    let bytes_image_music_video = include_bytes!(
         "../../../docker/core/mkwebaxum/static/image/listening-music-video-clip-with-auricular.png"
     );
-    let bytes_image_photo = include_bytes!("../../../docker/core/mkwebaxum/static/image/photo.png");
-    let bytes_image_radio = include_bytes!("../../../docker/core/mkwebaxum/static/image/radio.png");
-    let bytes_image_books = include_bytes!("../../../docker/core/mkwebaxum/static/image/books.png");
+    let bytes_image_photo =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/photo.png");
+    let bytes_image_radio =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/radio.png");
+    let bytes_image_books =
+        include_bytes!("../../../docker/core/mkwebaxum/static/image/books.png");
     let bytes_image_settings =
         include_bytes!("../../../docker/core/mkwebaxum/static/image/settings.png");
     let bytes_image_return =
@@ -48,90 +66,85 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let _server_list =
         mk_lib_network::mk_lib_network_mediakraken::mk_lib_network_find_mediakraken_server();
+
     let app = app::App::default().with_scheme(app::Scheme::Gleam);
-    let mut window_main = Window::default().with_size(800, 480); // pi 7" screen default
+
+    let theme = WidgetScheme::new(SchemeType::Fluent);
+    theme.apply();
+
+    // main menu window
     let mut window_menu = Window::default().with_size(800, 480);
-    // window_menu - left side buttons
-    let mut button_in_progress = Button::new(0, 0, 133, 96, "In Progress");
-    set_button_image(&mut button_in_progress, bytes_image_rectangle, 133, 96)?;
-    let mut button_new = Button::new(0, 96, 133, 96, "New");
-    set_button_image(&mut button_new, bytes_image_new, 133, 96)?;
-    let mut button_movie = Button::new(0, 192, 133, 96, "Movie");
-    set_button_image(&mut button_movie, bytes_image_movie_ticket, 133, 96)?;
-    let mut button_tv = Button::new(0, 288, 133, 96, "TV");
-    set_button_image(&mut button_tv, bytes_image_television, 133, 96)?;
-    let mut button_game = Button::new(0, 384, 133, 96, "Games");
-    set_button_image(&mut button_game, bytes_image_vid_game, 133, 96)?;
-    // window_menu - top middle button
-    let mut button_demo = Button::new(133, 0, 532, 384, "Demo");
-    set_button_image(&mut button_demo, bytes_image_theater, 532, 384)?;
-    // window_menu - bottom middle buttons
-    let mut button_music = Button::new(133, 384, 133, 96, "Music");
-    set_button_image(&mut button_music, bytes_image_headphone, 133, 96)?;
-    let mut button_live_tv = Button::new(266, 384, 133, 96, "Live TV");
-    set_button_image(&mut button_live_tv, bytes_image_television_live, 133, 96)?;
-    let mut button_home_video = Button::new(399, 384, 133, 96, "Home Video");
-    set_button_image(&mut button_home_video, bytes_image_vid_camera, 133, 96)?;
-    let mut button_internet = Button::new(532, 384, 133, 96, "Internet");
-    set_button_image(&mut button_internet, bytes_image_earth, 133, 96)?;
-    // window_menu - right side buttons
-    let mut button_music_video = Button::new(666, 0, 133, 96, "Music Video");
-    set_button_image(
-        &mut button_music_video,
-        bytes_image_listening_music_video_clip_with_auricular,
-        133,
-        96,
-    )?;
-    let mut button_pictures = Button::new(666, 96, 133, 96, "Pictures");
-    set_button_image(&mut button_pictures, bytes_image_photo, 133, 96)?;
-    let mut button_radio = Button::new(666, 192, 133, 96, "Radio");
-    set_button_image(&mut button_radio, bytes_image_radio, 133, 96)?;
-    let mut button_books = Button::new(666, 288, 133, 96, "Books");
-    set_button_image(&mut button_books, bytes_image_books, 133, 96)?;
-    let mut button_settings = Button::new(666, 384, 133, 96, "Settings");
-    set_button_image(&mut button_settings, bytes_image_settings, 133, 96)?;
+
+    // left column
+    let _button_in_progress =
+        make_image_button(0, 0, 133, 96, "In Progress", bytes_image_rectangle)?;
+    let _button_new =
+        make_image_button(0, 96, 133, 96, "New", bytes_image_new)?;
+    let _button_movie =
+        make_image_button(0, 192, 133, 96, "Movie", bytes_image_movie_ticket)?;
+    let _button_tv =
+        make_image_button(0, 288, 133, 96, "TV", bytes_image_television)?;
+    let _button_game =
+        make_image_button(0, 384, 133, 96, "Games", bytes_image_vid_game)?;
+
+    // center
+    let _button_demo =
+        make_image_button(133, 0, 532, 384, "Demo", bytes_image_theater)?;
+    let _button_music =
+        make_image_button(133, 384, 133, 96, "Music", bytes_image_headphone)?;
+    let _button_live_tv =
+        make_image_button(266, 384, 133, 96, "Live TV", bytes_image_television_live)?;
+    let _button_home_video =
+        make_image_button(399, 384, 133, 96, "Home Video", bytes_image_vid_camera)?;
+    let _button_internet =
+        make_image_button(532, 384, 133, 96, "Internet", bytes_image_earth)?;
+
+    // right column
+    let _button_music_video =
+        make_image_button(666, 0, 133, 96, "Music Video", bytes_image_music_video)?;
+    let _button_pictures =
+        make_image_button(666, 96, 133, 96, "Pictures", bytes_image_photo)?;
+    let _button_radio =
+        make_image_button(666, 192, 133, 96, "Radio", bytes_image_radio)?;
+    let _button_books =
+        make_image_button(666, 288, 133, 96, "Books", bytes_image_books)?;
+    let mut button_settings =
+        make_image_button(666, 384, 133, 96, "Settings", bytes_image_settings)?;
+
     window_menu.end();
     window_menu.make_resizable(true);
     window_menu.fullscreen(true);
 
+    // settings window
     let mut window_settings = Window::default().with_size(800, 480);
-    let mut button_settings_back = Button::new(666, 384, 133, 96, "Back");
-    set_button_image(&mut button_settings_back, bytes_image_return, 133, 96)?;
+    let mut button_settings_back =
+        make_image_button(666, 384, 133, 96, "Back", bytes_image_return)?;
     window_settings.end();
     window_settings.make_resizable(true);
     window_settings.fullscreen(true);
     window_settings.hide();
 
-    let theme = WidgetScheme::new(SchemeType::Fluent);
-    theme.apply();
+    // callbacks
+    {
+        let mut menu = window_menu.clone();
+        let mut settings = window_settings.clone();
+        button_settings.set_callback(move |_| {
+            menu.hide();
+            settings.show();
+        });
+    }
 
-    let mut window_media = Window::default().with_size(800, 480);
+    {
+        let mut menu = window_menu.clone();
+        let mut settings = window_settings.clone();
+        button_settings_back.set_callback(move |_| {
+            settings.hide();
+            menu.show();
+        });
+    }
 
-    window_media.end();
-    window_media.make_resizable(true);
-    window_media.fullscreen(true);
-    window_media.hide();
-
-    // close up the add to main window and setup current page
-    window_main.end();
-    window_main.make_resizable(true);
-    window_main.fullscreen(true);
-    window_main.show();
-    window_menu.make_current();
-
-    // // main button
-    // button_settings.set_callback( |_b| {
-    //     window_menu.hide();
-    //     window_settings.show();
-    // });
-    //
-    // // media list page
-    //
-    // // settings page
-    // button_settings_back.set_callback(move |bb| {
-    //     window_menu.show();
-    //     window_settings.hide();
-    // });
+    // show the real UI window
+    window_menu.show();
 
     app.run()?;
     Ok(())
