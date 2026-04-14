1ed2cf7532be6687153721e7dffd4e9f3d20d634
spootdev <spootdev@gmail.com>
2026-04-13 20:55:17 -0500

    ebug

--- docker/core/mkwebaxum/src/main.rs
+++ docker/core/mkwebaxum/src/main.rs
@@ -168,7 +168,7 @@ async fn main() {
             fmt::layer()
                 .with_target(true)
                 .with_ansi(false)
-                .with_filter(EnvFilter::builder().parse_lossy("mkwebapp=trace")),
+                .with_filter(EnvFilter::builder().parse_lossy("mkwebapp=debug,tower_http=debug")),
         )
         .init();
     tracing::info!("App start");
